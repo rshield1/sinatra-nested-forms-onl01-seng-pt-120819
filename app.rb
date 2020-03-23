557f901e8ab0e5523c28a0793acbda6759d67b69
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-23 18:38:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,24 @@ module FormsLab
 
     # code other routes/actions here
 
-  end
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |info|
+        Ship.new(info)
+      end
+      @@ships = Ship.all
+
+      
+      erb :'pirates/show'
+    end
+end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  @@all =[]
+  
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:type]
+    @height = args[:booty]
+    
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all = []
+  end
+  
 end
